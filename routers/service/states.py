b207dc122b2991457388b585d71493d41a50099e
Babahasko <zakutni4ek@gmail.com>
2024-11-28 22:43:29 +0300

    add Surveillance states

--- routers/service/states.py
+++ routers/service/states.py
@@ -3,3 +3,7 @@ from aiogram.fsm.state import StatesGroup, State
 class Theme(StatesGroup):
     name = State()
     delete = State()
+
+class Surveillance(StatesGroup):
+    name = State()
+    delete = State()
